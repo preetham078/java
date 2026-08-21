92f953661357e822f1b1aeba1ca6550604371185
Preetham.M <preetham.m0099@gmail.com>
2026-08-21 17:01:17 +0530

    Update overloding.java

--- overloding.java
+++ overloding.java
@@ -10,13 +10,26 @@ class calculator {
     double add(double a, double b) {
         return a + b;
     }
+    void display(){
+        System.out.println("This is basic calculator");
+    }
 }
+class advancedCalculator extends calculator 
+{   @Override
+    void display(){
+        System.out.println("This is advanced calculator");
+    }
+}
+
 
 public class overloding {
     public static void main(String[] args) {
         calculator c = new calculator();
+        c.display();
+        advancedCalculator ac = new advancedCalculator();
         System.out.println(c.add(10, 20));
         System.out.println(c.add(10, 20, 30));
         System.out.println(c.add(10.5, 20.5));
+        ac.display();
     }
 }
