4c48b33af8ccaaefa0c9b975f1faebacb04fc3cd
Preetham.M <preetham.m0099@gmail.com>
2026-09-23 10:19:07 +0530

    college

--- Main.java
+++ Main.java
@@ -1,19 +1,10 @@
-package bank;
-import bank.accounts.Account;
-import bank.customer.Customer;
-import bank.loan.Loan;
+package collage;
+import collage.student.Student;
 
 public class Main {
     public static void main(String[] args) {
-        Customer customer = new Customer(1, "John Doe", 1234567890.0);
-        Account account = new Account(101, "Savings", 1000.0);
-        Loan loan = new Loan(1001, "Home", 50000.0);
-
-        customer.display();
-        account.display();
-        loan.display();
-        account.deposit(500.0);
-        account.withdraw(200.0);
-        account.displayBalance();
+        Student s = new Student();
+        s.display();
     }
+    
 }
---
+++ Student.java
@@ -0,0 +1,8 @@
+package collage.student;
+
+public class Student {
+    public void display() {
+        System.out.println("student details displayed");
+    }
+    
+}
